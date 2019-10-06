3c9dc747af47d55e6a67c2879ea5dad450b949bf
Marcelo Madruga <marcelomadruga@gmail.com>
2019-10-05 21:05:00 -0400

    Merge with 0.0.2

--- src/main/kotlin/io/expo/sdk/PushException.kt
+++
@@ -1,6 +0,0 @@
-package io.expo.sdk
-
-class PushException(val endpoint: String,
-                    val payload: String,
-                    message: String? = null,
-                    cause: Throwable? = null) : RuntimeException(message, cause)
